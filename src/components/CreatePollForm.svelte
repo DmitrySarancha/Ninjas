f2b7b6a67964d56031088277345d97b9b9461cfe
Dmitry Sarancha <lottyskound@gmail.com>
2022-04-02 11:10:13 +0300

    add validate form

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -2,12 +2,47 @@
   import Button from '../shared/Button.svelte';
   let fields = {
     question: '',
-    'answer-a': '',
-    'answer-b': '',
+    answerA: '',
+    answerB: '',
+  };
+  let errors = {
+    question: '',
+    answerA: '',
+    answerB: '',
   };
 
+  let valid = false;
+
   const submitHandel = () => {
-    console.log(fields);
+    valid = true;
+
+    // validate question
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = 'Question must be at 5 characters long';
+    } else {
+      errors.question = '';
+    }
+
+    // validate ansver-a
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = 'Answer A cannot be ampty';
+    } else {
+      errors.answerA = '';
+    }
+
+    // validate answer-b
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = 'Answer B cannot be ampty';
+    } else {
+      errors.answerB = '';
+    }
+
+    if (valid) {
+      console.log('valid', fields);
+    }
   };
 </script>
 
@@ -20,6 +55,8 @@
       id="question"
       bind:value={fields.question}
     />
+    <!-- not valid question -->
+    <div class="error">{errors.question}</div>
   </div>
 
   <div class="form-field">
@@ -28,8 +65,10 @@
       type="text"
       name="answer-a"
       id="answer-a"
-      bind:value={fields['answer-a']}
+      bind:value={fields.answerA}
     />
+    <!-- not valid answer A -->
+    <div class="error">{errors.answerA}</div>
   </div>
 
   <div class="form-field">
@@ -38,8 +77,10 @@
       type="text"
       name="answer-b"
       id="answer-b"
-      bind:value={fields['answer-b']}
+      bind:value={fields.answerB}
     />
+    <!-- not valid answer B -->
+    <div class="error">{errors.answerB}</div>
   </div>
 
   <Button flat={true}>Add Poll</Button>
@@ -61,5 +102,10 @@
   label {
     margin-bottom: 10px;
     text-align: left;
+  }
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
   }
 </style>
