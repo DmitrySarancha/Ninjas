f2e18b02ad85419ee1fec61208d4cdcbc2392954
Dmitry Sarancha <lottyskound@gmail.com>
2022-04-03 22:17:04 +0300

    start template

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,12 @@
 import { writable } from 'svelte/store';
 
-export const pollsStore = writable([]);
+export const pollsStore = writable([
+  {
+    id: 1,
+    question: 'Vue or React?',
+    answerA: 'vue...',
+    answerB: 'only React!',
+    votesA: 0,
+    votesB: 0,
+  },
+]);
