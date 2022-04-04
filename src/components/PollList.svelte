44c2e663602d146d713e185a484c4d71cc536d26
Dmitry Sarancha <lottyskound@gmail.com>
2022-04-04 20:09:30 +0300

    add animation and transition

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,11 +1,15 @@
 <script>
+  import { fade, scale } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
   import { pollsStore } from '../store/store';
   import PollDetails from './PollDetails.svelte';
 </script>
 
 <div class="poll-list">
   {#each $pollsStore as item (item.id)}
-    <PollDetails {item} />
+    <div animate:flip={{ duration: 300 }} in:fade out:scale|local>
+      <PollDetails {item} />
+    </div>
   {/each}
 </div>
 
